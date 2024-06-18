1a438367addbe8335ee049f0d69d6185c3011df9
griceldavaldez <gvaldez@sodep.com.py>
2024-06-18 15:06:00 -0400

    Implement Provider with state

--- flutter-riverpod-app/lib/config/helpers/random_generator.dart
+++ flutter-riverpod-app/lib/config/helpers/random_generator.dart
@@ -1,10 +1,7 @@
-
-
-
 import 'package:random_name_generator/random_name_generator.dart';
 
-class RandomGenerator {
 
+class RandomGenerator {
 
   static String getRandomName() {
     final randomNames = RandomNames(Zone.spain);
