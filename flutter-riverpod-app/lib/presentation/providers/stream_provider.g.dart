9fb0dd624b365c3c3ef465bf4b8a992ba5556c1c
griceldavaldez <gvaldez@sodep.com.py>
2024-06-20 16:56:43 -0400

    Implement stream provider

---
+++ flutter-riverpod-app/lib/presentation/providers/stream_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'stream_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$usersInChatHash() => r'1348d808b8e8d59a79dcb36d3da0ea716e339988';
+
+/// See also [usersInChat].
+@ProviderFor(usersInChat)
+final usersInChatProvider = StreamProvider<List<String>>.internal(
+  usersInChat,
+  name: r'usersInChatProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$usersInChatHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef UsersInChatRef = StreamProviderRef<List<String>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
